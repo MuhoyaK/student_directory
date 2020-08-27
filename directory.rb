132d9fe8ef20c8e1e22f3078252ecf2ba902bdef
kiiru <kiirumuhoya2@gmail.com>
2020-08-27 10:25:38 +0300

    Refactoring to get user-input

--- directory.rb
+++ directory.rb
@@ -1,17 +1,21 @@
-#   Let's put all students into an array
-students = [
-  {name: "Dr.Hannibal Lectar", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baraheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+# getting user input
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "now we have #{students.count} students"
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
 # And then print them
 def print_header
   puts "The students of Villains Academy"
@@ -25,6 +29,7 @@ end
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
+students = input_students
 # nothing happens untill we call the methods
 print_header
 print(students)
