a88fa16e1f131b7acd07ff8803f83549ed0a441a
kiiru <kiirumuhoya2@gmail.com>
2020-08-27 07:02:31 +0300

    Total Number of Students

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# First we print the list of students
 puts "The students of Villains Academy"
 puts "_____________"
 puts "Dr.Hannibal Lectar"
@@ -10,4 +11,9 @@ puts "Terminator"
 puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baraheon"
+# Finally we print the total number of students
 puts "Norman Bates"
+print "Overall, we have "
+# It's important the print doesn't add new line characters
+print 11
+puts " great students"
