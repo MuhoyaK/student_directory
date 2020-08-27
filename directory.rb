22629cea3471aaad93dc74f8564ea471ff2a8a83
Kiiru Muhoya <nicholasmuhoya@Nicholass-MacBook-Pro.local>
2020-08-28 00:13:36 +0300

    Added a saved file method

--- directory.rb
+++ directory.rb
@@ -21,6 +21,7 @@ end
 def print_menu
   puts "1. Input students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -30,12 +31,26 @@ def show_students
   print_footer
 end
 
+def save_students
+  # open file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def process(selection)
   case selection
     when "1"
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
