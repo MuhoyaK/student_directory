447a31113b5797a292e864cc4e23ebfb0aef6449
kiiru <kiirumuhoya2@gmail.com>
2020-08-27 23:18:25 +0300

    Refactoring to use more methods

--- directory.rb
+++ directory.rb
@@ -1,36 +1,60 @@
-# getting user input
+@students = []
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
-  # get the first name
   name = gets.chomp
-  # while the name is not empty repeat this code
   while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "now we have #{@students.count} students"
     name = gets.chomp
   end
-  # return the array of students
-  students
 end
-# And then print them
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+  end
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "_____________"
 end
-def print(students)
-  students.each do |student|
+
+def print_student_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
-students = input_students
-# nothing happens untill we call the methods
-print_header
-print(students)
-print_footer(students)
+interactive_menu
