c8010b27e73d897571f25296c119823cc64e6190
Kiiru Muhoya <nicholasmuhoya@Nicholass-MacBook-Pro.local>
2020-08-28 11:42:27 +0300

    Added a load file option

--- directory.rb
+++ directory.rb
@@ -22,6 +22,7 @@ def print_menu
   puts "1. Input students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -43,6 +44,16 @@ def save_students
   file.close
 end
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+  @students << {name: name, cohort: cohort.to_sym}
+    puts @students
+  end
+  file.close
+end
+
 def process(selection)
   case selection
     when "1"
@@ -51,6 +62,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
